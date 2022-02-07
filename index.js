d90ac9fec796235cda8eea8cd123a1ebb0d9980c
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-07 20:55:26 -0300

    let anidado

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // document.getElementById("count-el").innerText = 5
 
-function increment() {
-    console.log("The button wascliked")
+// function increment() {
+//     console.log("The button wascliked")
+// }
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function totalRace(){
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
 }
+
+totalRace()
