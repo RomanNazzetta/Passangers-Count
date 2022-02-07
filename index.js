e26051abe87bbed0da25db1aa0ce989ef8e717eb
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-07 20:11:44 -0300

    conole.log myAge

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+let count = 0
+
+console.log(count)
+
+let myAge = 35
+
+console.log(myAge)
