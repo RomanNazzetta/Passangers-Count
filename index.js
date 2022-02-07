04ead0c3940e9645d902b3d01d55e2e1e461dbfd
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-07 20:41:42 -0300

    function onclick

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // document.getElementById("count-el").innerText = 5
 
-let bonusPoints = 50
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+function increment() {
+    console.log("The button wascliked")
+}
