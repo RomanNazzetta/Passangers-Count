f820fa2f5e24e39ff3fc247cb44695da85489806
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-08 18:13:12 -0300

    save btn

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// document.getElementById("count-el").innerText = 5
-
-// function increment() {
-//     console.log("The button wascliked")
-// }
-
 let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment (){
     count = count + 1
     countEl.innerText = count
+}
+
+function save (){
+    console.log(count)
 }
