7f677865ca1f2cad05a25a984dd172cccbd5db95
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-08 18:28:10 -0300

    variable string

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+// let countEl = document.getElementById("count-el")
+// let count = 0
 
-function increment (){
-    count = count + 1
-    countEl.innerText = count
-}
+// function increment (){
+//     count = count + 1
+//     countEl.innerText = count
+// }
 
-function save (){
-    console.log(count)
-}
+// function save (){
+//     console.log(count)
+// }
+
+
+let myName = "Roman"
+let greeting = "Hi, mi name is "
+let myGreeting = greeting + myName
+console.log(myGreeting)
